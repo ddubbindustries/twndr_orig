5b4813f00f6ef2450b5795d75a950ae5a051b0fa
D W <ddubb@protos.(none)>
2012-10-30 17:42:34 +0000

    added sample apikeys file

---
+++ srvr/apikeys-sample.js
@@ -0,0 +1,8 @@
+// twitter api
+exports.consumerKey =		'abc';
+exports.consumerSecret =	'xyz';
+exports.token = 			'123';
+exports.secret = 			'ijk';
+
+// mailgun api
+exports.mailgunkey = 		'api';
